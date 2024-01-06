2a7e8433414d6c886799fbd934c080d0305f8c94
DoddapaneniMahitha <155796149+DoddapaneniMahitha@users.noreply.github.com>
2024-01-06 12:55:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 dataset=pd.read_csv('crop.csv')
 X=dataset.iloc[:,:-1].values
 Y=dataset.iloc[:,-1].values
-X_train,X_test.Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
+X_train,X_test,Y_train,Y_test=train_test_split(X,Y,test_size=0.2)
 classifier=LogisticRegression()
 classifire.fit(X_train,Y_train)
 
